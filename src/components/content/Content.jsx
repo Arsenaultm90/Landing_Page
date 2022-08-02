796b006f5be49a87029a1614c30a982189c20900
Matt Arsenault <Matthew.B.Arsenault@gmail.com>
2022-08-02 17:37:33 -0400

    Added better repeating background. Divided the content screen between the image and the text.

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import '../../global.css'
-import './Content.css'
-import picklerLogo from '../../images/pickleLogo.jpg'
+import React from "react";
+import "../../global.css";
+import "./Content.css";
+import picklerLogo from "../../images/pickleLogo.jpg";
 
 const Content = () => {
   return (
-    <section className="content">
-      <div className="content-container">
-        <div className="img-container">
-          <img src={picklerLogo} alt="" />
+    <section className='content'>
+      <div className='content-container'>
+        <div className='img-container'>
+          <img src={picklerLogo} alt='' />
         </div>
-        <div className="text-container">
+        <div className='text-container'>
           <h1>Experience A Classic</h1>
-          <p>A game made to challenge the player. Experience jaw-dropping graphics when you boot up this bad boy in MS-DOS.</p>
+          <br />
+          <p>
+            A game made to challenge the player.
+            Experience jaw-dropping graphics when
+            you boot up this bad boy in MS-DOS.
+            <br />
+            <br />
+            On the peaceful planet of Arcadia you
+            play as Dave and Linda on the search
+            for weapons to prevent the invasion.
+            Though the planet has not needed
+            weapons for centuries there is a man
+            known as Lord Geric who knows of a
+            Doomsday device that will end the
+            invasion.
+          </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
