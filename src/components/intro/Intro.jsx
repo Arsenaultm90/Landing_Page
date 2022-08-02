98c265ce1851cd00ac3b1139b9d21c6efa432179
Matt Arsenault <Matthew.B.Arsenaultt@gmail.com>
2022-08-02 12:05:59 -0400

    Added intro page with playing video

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import '../../global.css'
 import './Intro.css'
+import monitor from '../../images/monitor.png'
 
 const Intro = () => {
   return (
-    <section className="intro" id ="intro"></section>
+    <section className="intro" id ="intro">
+      <div className="intro-container">
+        <div className="video-container">
+          <iframe width="430" height="380" src="https://www.youtube.com/embed/Ds3HdJiyyMk?controls=0&rel=0&autoplay=1&loop=1&mute=1&playlist=Ds3HdJiyyMk" title="YouTube video player" frameBorder="0" allow="autoplay"></iframe>
+          <img src={monitor} alt="" />
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
-import '../../global.css'
-import './Navbar.css'
+import React from "react";
+import "../../global.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-   <section className="navbar">
-    <div className="navbar-container">
-      <div className="left">
-        <h1>Pickle Wars</h1>
-        <h4>by Redwood Games</h4>
+    <section className='navbar'>
+      <div className='navbar-container'>
+        <div className='left'>
+          <h1>Pickle Wars</h1>
+          <h5>by Redwood Games</h5>
+        </div>
+        <nav className='right'>
+          <ul className='list-items'>
+            <li className='list-item'>
+              <a href='#intro'>Home</a>
+            </li>
+            <li className='list-item'>
+              <a href='#content'>Content</a>{" "}
+            </li>
+            <li className='list-item'>
+              <a href='#specification'>
+                Specification
+              </a>
+            </li>
+            <li className='list-item purchase-btn'>
+              <button>
+                <a href='#purchase'>Purchase</a>
+              </button>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="right">
-        <ul className="list-items">
-          <li className="list-item"><a href="#intro">Home</a></li>
-          <li className="list-item">Content</li>
-          <li className="list-item"><a href="#specification">Specification</a></li>
-          <li className="list-item"><span>Purchase</span></li>
-        </ul>
-      </nav>
-    </div>
-   </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Navbar
+export default Navbar;
