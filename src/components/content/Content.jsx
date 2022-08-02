4f52f2319e2db13bc751198e3cd981e3e8a3d7c5
Matt Arsenault <Matthew.B.Arsenaultt@gmail.com>
2022-08-02 13:33:34 -0400

    Added moving background. Added rotating image to content page. Added descriptions.

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../../global.css'
 import './Content.css'
+import picklerLogo from '../../images/pickleLogo.jpg'
 
 const Content = () => {
   return (
-    <section className="content"></section>
+    <section className="content">
+      <div className="content-container">
+        <div className="img-container">
+          <img src={picklerLogo} alt="" />
+        </div>
+        <div className="text-container">
+          <h1>Experience A Classic</h1>
+          <p>A game made to challenge the player. Experience jaw-dropping graphics when you boot up this bad boy in MS-DOS.</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
